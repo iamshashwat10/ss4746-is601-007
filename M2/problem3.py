adb024242eec7d4a314355da8be5ebadf75a69e9
iamshashwat10 <shashwat.singh0628@gmail.com>
2023-09-25 16:19:56 -0400

    completed hw

--- M2/problem3.py
+++ M2/problem3.py
@@ -11,7 +11,28 @@ def process_array(num, arr):
     # Note: use the arr variable; don't directly refer to a1-a4 variables
     # TODO add new code here to print the desired result
     # TODO include the type() of the output data to ensure the result is positive AND the same datatype as the input value
+    #ss4746_Shashwat_Singh-09/23/23
+    
+    pos_arr = []
+    for i in arr:
+        if isinstance(i, str):
+            pos_arr.append(abs(int(i)))
+        else:
+            pos_arr.append(abs(i))
 
+    # To Keep the output data type and the input data type same
+    if all(isinstance(z, int) for z in arr):
+        pos_arr = [int(z) for z in pos_arr]
+    elif all(isinstance(z, float) for z in arr):
+        pos_arr = [float(z) for z in pos_arr]
+    elif all(isinstance(z, str) for z in arr):
+        pos_arr = [str(z) for z in pos_arr]
+
+    # Printing the positive output array
+    print(pos_arr)
+
+    # Printing output data type
+    print("\nType of output data:", type(pos_arr[0]))
 
 print("Problem 3")
 process_array(1, a1)
